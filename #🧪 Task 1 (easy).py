f9f577523e4c5e54f229f433a15456b00379b002
Joshua Levy <joshualevy84@gmail.com>
2026-05-03 11:46:28 +0100

    practice

---
+++ #🧪 Task 1 (easy).py
@@ -0,0 +1,23 @@
+#🧪 Task 1 (easy)
+
+#👉 Start at 2
+#👉 Add 2 each time
+#👉 Stop when number is greater than 10
+#👉 Print values
+
+number = 2
+while number <= 10:
+    print(number)
+    number = number + 2
+
+#🧪 Task 2 (reverse)
+
+#👉 Start at 7
+#👉 Subtract 2
+#👉 Stop when number is less than 1
+#👉 Print values
+
+number = 7
+while number >= 1:
+    print(number)
+    number = number - 2

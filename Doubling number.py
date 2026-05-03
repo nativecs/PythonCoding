2dc5a25e2646e29b7e99e1c38a98494993b743e5
Joshua Levy <joshualevy84@gmail.com>
2026-05-03 11:46:16 +0100

    practice

---
+++ Doubling number.py
@@ -0,0 +1,11 @@
+# doubling up number and stop when it exceeds 20. 
+
+number = 1
+while  number <= 20:
+    number = number * 2
+    print(number)
+
+# if we use the PRINT outside the loop it will only show the final number and not the iteration. 
+
+
+
